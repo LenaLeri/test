f3ae2d43cca972693777b12a9594c72cd5ce207c
Yelena Leri <yelenaleri@gmail.com>
2022-07-29 21:20:14 +0300

    add

---
+++ test/js/test.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[3];
+
+console.log(fruits.length - 1);
+console.log(fruits[fruits.length - 1]);
